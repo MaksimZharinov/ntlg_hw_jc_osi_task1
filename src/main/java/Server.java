9ecbd75c6540594fdfb8796a12ada52cf9df75d4
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-03-24 00:03:42 +0700

    create Server

---
+++ src/main/java/Server.java
@@ -0,0 +1,29 @@
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static final Integer PORT = 8888;
+
+    public static void main(String[] args) throws IIOException {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println(String.format("Start server. Port: %d", PORT));
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                     String infoFromClient = in.readLine();
+                    System.out.printf("New client is connect. Info: %s\nclient port: %d",
+                            infoFromClient, clientSocket.getPort());
+                }
+            }
+        } catch (IOException e) {
+            new RuntimeException(e);
+        }
+    }
+}
